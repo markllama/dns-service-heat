9214e06bce3984192afbc6b7f45f76edb19e4bcd
Mark Lamourine <markllama@gmail.com>
2017-03-06 09:08:34 -0500

    variable key file location

--- bin/full_service.sh
+++ bin/full_service.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+PRIVATE_KEY_FILE=${PRIVATE_KEY_FILE:-~/.ssh/dns_stack_key}
+[ -r $PRIVATE_KEY_FILE ] || (echo no key file $PRIVATE_KEY_FILE && exit 1)
+
 openstack stack create -e dns_service_parameters.yaml -e rhn_credentials.yaml   -t dns_service.yaml  dns-service
 
 function stack_complete() {
@@ -7,7 +10,6 @@ function stack_complete() {
 		[ $(openstack stack show $1 -f json | jq '.stack_status' | tr -d \") == "CREATE_COMPLETE" ]
 }
 
-START=$(date +%s)
 POLL_TRY=0
 POLL_INTERVAL=5
 while ! stack_complete dns-service ; do
@@ -15,15 +17,11 @@ while ! stack_complete dns-service ; do
 		sleep $POLL_INTERVAL
 		POLL_TRY=$(($POLL_TRY + 1))
 done
-END=$(date +%s)
-echo "Stack create took $(($END - $START)) seconds" 
 
 python bin/stack_data.py --zone dns.example.com --update-key bKcZ4P2FhWKRQoWtx5F33w== >dns_stack_data.yaml
 
 jinja2-2.7 ansible/inventory.j2 dns_stack_data.yaml > inventory
 
-exit
-
-ansible-playbook -i inventory --become --user cloud-user --private-key ../keys/ocp3_rsa --ssh-common-args "-o StrictHostKeyChecking=no" ../dns-service-playbooks/playbooks/bind-server.yml
+ansible-playbook -i inventory --become --user cloud-user --private-key ${PRIVATE_KEY_FILE} --ssh-common-args "-o StrictHostKeyChecking=no" ../dns-service-playbooks/playbooks/bind-server.yml
 
 python bin/prime_slave_servers.py
