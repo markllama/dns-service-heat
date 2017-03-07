72a0a01205dc245a334abae87e1cd831aeccade2
Mark Lamourine <markllama@gmail.com>
2017-03-07 08:24:14 -0500

    gathering stuff from the stack to create DNS entries

--- bin/ocp_hosts.py
+++ bin/ocp_hosts.py
@@ -29,13 +29,15 @@ def parse_cli():
     opts.add_option("-P", "--project", default=os.environ['OS_TENANT_NAME'])
     opts.add_option("-U", "--auth-url", default=os.environ['OS_AUTH_URL'])
     opts.add_option("-n", "--network", default="dns-network")
+    opts.add_option("-m", "--nameserver")
+    opts.add_option("-k", "--update-key", default=os.getenv('UPDATE_KEY'))
+    opts.add_option("-S", "--stack", default="ocp3")
     opts.add_option("-z", "--zone", default="example.com")
     opts.add_option("-s", "--subzone", default="control")
     opts.add_option("-M", "--master-pattern", default="master")
     opts.add_option("-I", "--infra-pattern", default="infra")
     opts.add_option("-N", "--node-pattern", default="node")
 #    opts.add_option("-s", "--slave-prefix", default="ns")
-#    opts.add_option("-k", "--update-key", default=os.getenv('UPDATE_KEY'))
 
     return opts.parse_args()
 
@@ -62,13 +64,6 @@ def add_a_record(name,zone,ipv4addr,master,key):
     response = dns.query.tcp(update, master)
     return response
 
-def add_ns_record(zone,master,key):
-    keyring = dns.tsigkeyring.from_text({'update-key': key})
-    update = dns.update.Update(zone, keyring=keyring)
-    update.replace(zone, 300, 'ns', fqdn)
-    response = dns.query.tcp(update, master)
-    return response
-
 def host_part(fqdn,zone):
     zone_re = re.compile("(.*).(%s)$" % zone)
     response = zone_re.match(fqdn)
@@ -88,28 +83,22 @@ if __name__ == "__main__":
 
     
     #print nova.servers.list()[0].addresses[opts.network]
-    pairs = [fixed_ip(server, opts.network) for server in nova.servers.list()]
-    print pairs
-    
-#    master_addr = [host['address'] for host in pairs if host_part(host['name'], opts.zone) == opts.master]
-
-#    print master_addr[0]
+    pairs = [fixed_ip(server, opts.network) for server in nova.servers.list() if fixed_ip(server,opts.network) is not None]
+    #print pairs
     
     #set_a_record(pairs
-    #print pairs
-#    for record in pairs:
-#        add_a_record(
-#            host_part(record['name'], opts.zone),
-#            opts.zone,
-#            record['address'],
-#            master_addr[0],
-#            opts.update_key
-#        )
-#        add_ns_record(
-#           host_part(record['name'], opts.zone), 
-#           opts.zone,
-#            master_addr[0],
-#            opts.update_key
-#        )
+    print pairs
+    print "---"
+    for record in pairs:
+        print record
+        print opts.nameserver
+        print opts.update_key
+        add_a_record(
+            host_part(record['name'], opts.zone),
+            opts.zone,
+            record['address'],
+            opts.nameserver,
+            opts.update_key
+        )
                 
     
